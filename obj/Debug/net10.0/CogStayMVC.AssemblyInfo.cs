4f426e2ccfb47d44566f114f660a3515232ab0c8
Ritesh Sanjay Kulkarni <kulkarniritesh54@gmail.com>
2026-06-26 01:09:07 +0530

    Available Room Page Updated

--- obj/Debug/net10.0/CogStayMVC.AssemblyInfo.cs
+++ obj/Debug/net10.0/CogStayMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CogStayMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8655f4032246b89776cd7845639f1617b0aecb33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38f7e9828a856d13cfd3f0b54e3f775c3ee83b3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CogStayMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CogStayMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
